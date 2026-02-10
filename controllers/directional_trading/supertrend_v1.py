f0640860260cd1b55e45138f4238e981591b62a8
cardosofede <federico.cardoso.e@gmail.com>
2026-02-10 18:42:04 -0300

    (feat) remove candles config from directional trading

--- controllers/directional_trading/supertrend_v1.py
+++ controllers/directional_trading/supertrend_v1.py
@@ -5,6 +5,7 @@ from pydantic import Field, field_validator
 from pydantic_core.core_schema import ValidationInfo
 
 from hummingbot.data_feed.candles_feed.data_types import CandlesConfig
+
 from hummingbot.strategy_v2.controllers.directional_trading_controller_base import (
     DirectionalTradingControllerBase,
     DirectionalTradingControllerConfigBase,
@@ -13,7 +14,6 @@ from hummingbot.strategy_v2.controllers.directional_trading_controller_base impo
 
 class SuperTrendConfig(DirectionalTradingControllerConfigBase):
     controller_name: str = "supertrend_v1"
-    candles_config: List[CandlesConfig] = []
     candles_connector: str = Field(
         default=None,
         json_schema_extra={
@@ -56,13 +56,6 @@ class SuperTrend(DirectionalTradingControllerBase):
     def __init__(self, config: SuperTrendConfig, *args, **kwargs):
         self.config = config
         self.max_records = config.length + 10
-        if len(self.config.candles_config) == 0:
-            self.config.candles_config = [CandlesConfig(
-                connector=config.candles_connector,
-                trading_pair=config.candles_trading_pair,
-                interval=config.interval,
-                max_records=self.max_records
-            )]
         super().__init__(config, *args, **kwargs)
 
     async def update_processed_data(self):
@@ -86,3 +79,11 @@ class SuperTrend(DirectionalTradingControllerBase):
         # Update processed data
         self.processed_data["signal"] = df["signal"].iloc[-1]
         self.processed_data["features"] = df
+
+    def get_candles_config(self) -> List[CandlesConfig]:
+        return [CandlesConfig(
+            connector=self.config.candles_connector,
+            trading_pair=self.config.candles_trading_pair,
+            interval=self.config.interval,
+            max_records=self.max_records
+        )]
