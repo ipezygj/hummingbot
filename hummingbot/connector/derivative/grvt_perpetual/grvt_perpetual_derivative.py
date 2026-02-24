086d6e660d83d88b705f2c218d2832604f24d4ad
ipezygj <ipezygj2@gmail.com>
2026-02-24 13:34:05 +0200

    Create grvt_perpetual_derivative.py

---
+++ hummingbot/connector/derivative/grvt_perpetual/grvt_perpetual_derivative.py
@@ -0,0 +1,12 @@
+rom decimal import Decimal
+from typing import Any, Dict, List, Optional, Tuple
+from hummingbot.core.data_type.common import OrderType, TradeType
+from hummingbot.core.data_type.in_flight_order import InFlightOrder
+from hummingbot.connector.derivative.derivative_base import DerivativeBase
+from hummingbot.connector.derivative.grvt_perpetual.grvt_perpetual_auth import GrvtPerpetualAuth
+
+class GrvtPerpetualDerivative(DerivativeBase):
+def init(self, auth: GrvtPerpetualAuth, trading_pairs: List[str]):
+super().init()
+self._auth = auth
+self._trading_pairs = trading_pairs
