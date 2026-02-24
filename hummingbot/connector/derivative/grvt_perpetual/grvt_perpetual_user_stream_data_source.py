b03049133751f53fb58f9fb5f54c64b0a7a2983c
ipezygj <ipezygj2@gmail.com>
2026-02-24 12:40:37 +0200

    Implement GrvtPerpetualUserStreamDataSource class
    Added GrvtPerpetualUserStreamDataSource class to handle user data stream.

--- hummingbot/connector/derivative/grvt_perpetual/grvt_perpetual_user_stream_data_source.py
+++ hummingbot/connector/derivative/grvt_perpetual/grvt_perpetual_user_stream_data_source.py
@@ -1,1 +1,11 @@
+import asyncio
+from hummingbot.core.data_type.user_stream_tracker_data_source import UserStreamTrackerDataSource
+from hummingbot.connector.derivative.grvt_perpetual.grvt_perpetual_auth import GrvtPerpetualAuth
 
+class GrvtPerpetualUserStreamDataSource(UserStreamTrackerDataSource):
+"""
+Handles user data stream from GRVT exchange.
+"""
+def init(self, auth: GrvtPerpetualAuth):
+super().init()
+self._auth = auth
