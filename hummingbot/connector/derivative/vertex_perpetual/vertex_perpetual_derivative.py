f267c50620804ab747680820d177458d4b403606
ipezygj <ipezygj2@gmail.com>
2026-02-24 14:01:17 +0200

    Implement VertexPerpetualDerivative class

---
+++ hummingbot/connector/derivative/vertex_perpetual/vertex_perpetual_derivative.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+from typing import Any, Dict, List, Optional, Tuple
+from hummingbot.core.data_type.common import OrderType, TradeType
+from hummingbot.core.data_type.in_flight_order import InFlightOrder
+from hummingbot.connector.derivative.derivative_base import DerivativeBase
+from hummingbot.connector.derivative.vertex_perpetual.vertex_perpetual_auth import VertexPerpetualAuth
+
+class VertexPerpetualDerivative(DerivativeBase):
+def init(self, auth: VertexPerpetualAuth, trading_pairs: List[str]):
+super().init()
+self._auth = auth
+self._trading_pairs = trading_pairs
