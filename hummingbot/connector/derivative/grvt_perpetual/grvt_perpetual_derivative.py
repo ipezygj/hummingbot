a8cae78691392b58dc73d4a58eba71b66eaf2def
ipezygj <ipezygj2@gmail.com>
2026-02-24 13:44:36 +0200

    Fix import statement for Decimal module

--- hummingbot/connector/derivative/grvt_perpetual/grvt_perpetual_derivative.py
+++ hummingbot/connector/derivative/grvt_perpetual/grvt_perpetual_derivative.py
@@ -1,4 +1,4 @@
-rom decimal import Decimal
+from decimal import Decimal
 from typing import Any, Dict, List, Optional, Tuple
 from hummingbot.core.data_type.common import OrderType, TradeType
 from hummingbot.core.data_type.in_flight_order import InFlightOrder
